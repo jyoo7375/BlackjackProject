b62380142d5e3776153c4cc16601f94eb2825fac
Jinyoung Yoon <jyoo7375@gmail.com>
2025-03-28 16:43:44 -0400

    initial commit

---
+++ src/com/skilldistillery/cards/common/Deck.java
@@ -0,0 +1,35 @@
+package com.skilldistillery.cards.common;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+	
+	//DO NOT GENERATE A GETTER FOR CARDS
+	
+	private List<Card> cards;
+	
+	public Deck() {
+		cards = new ArrayList<>(52);
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				cards.add(new Card(suit, rank));
+			}
+		}
+	}
+	public int checkDeckSize() {
+		return cards.size();
+	}
+	public Card dealCard() {
+		if(checkDeckSize() < 1) {
+			return null;
+		}
+		return cards.remove(0);	
+	}
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+
+}
+
